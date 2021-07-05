28574f3e2c1414f62a16efd1d78267e8427a190d
asyraffff <amirulasyraf4.0x@gmail.com>
2021-07-05 14:41:10 +0800

    Add favorites hook

---
+++ hooks/favorites.js
@@ -0,0 +1,15 @@
+import { useLocalStorage } from './localStorage';
+
+const useFavorites = () => {
+  const [favorites, setFavorites] = useLocalStorage({ key: 'favorites', defaultValue: {}});
+
+  const toggleFavorite = (id) => {
+    const change = { ...favorites };
+    change[id] === undefined ? change[id] = true : delete change[id];
+    setFavorites(change);
+  };
+
+  return [favorites, toggleFavorite];
+};
+
+export { useFavorites };
